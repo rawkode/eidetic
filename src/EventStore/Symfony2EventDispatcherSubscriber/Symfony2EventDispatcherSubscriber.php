2d4171d396485ba9c6f293fe7be6897263abe154
David McKay <david@rawkode.com>
2015-11-28 11:39:45 +0000

    Fixed extra blank line

--- src/EventStore/Symfony2EventDispatcherSubscriber/Symfony2EventDispatcherSubscriber.php
+++ src/EventStore/Symfony2EventDispatcherSubscriber/Symfony2EventDispatcherSubscriber.php
@@ -4,7 +4,6 @@ namespace Rawkode\Eidetic\EventStore\Symfony2EventDispatcherSubscriber;
 
 use Rawkode\Eidetic\EventStore\EventStore;
 use Rawkode\Eidetic\EventStore\EventSubscriber;
-
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 final class Symfony2EventDispatcherSubscriber implements EventSubscriber
