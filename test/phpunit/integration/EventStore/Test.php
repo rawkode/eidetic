13ba25a9329121e2842a325b0058e63a1f38f9a9
David McKay <david@teamrock.com>
2015-12-03 15:34:10 +0000

    Suppose I should add the test back, though

---
+++ test/phpunit/integration/EventStore/Test.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rawkode\Eidetic\Tests;
+
+use Rawkode\Eidetic\EventStore\DBALEventStore\DBALEventStore;
+
+final class Test extends EventStoreTest
+{
+    /**
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->eventStore = DBALEventStore::createWithOptions('events', [
+            'driver' => 'pdo_sqlite',
+            'memory' => true,
+        ]);
+
+        $this->eventStore->createTable();
+    }
+
+    /**
+     */
+    public function tearDown()
+    {
+        $this->eventStore->dropTable();
+    }
+}
